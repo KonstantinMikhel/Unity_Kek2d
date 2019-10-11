03789e38c5212927085c380f30309b20270e1d6d
Agafiy_Poluchlebnik <zhuravlevyuri2000@gmail.com>
2019-10-11 21:23:57 +0300

    first day

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+	
+	public float speed = 1f;
+
+	public Rigidbody2D rb;
+
+	public GameObject ground;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		if (ground == null) {
+			ground = GameObject.FindWithTag ("Ground");
+		}
+	}
+
+	void Update () {
+
+		//CharacterController controller = GetComponent<CharacterController>();
+		// OnCollisionEnter
+
+		Vector2 force = new Vector2 (0.0f, 15000.0f); 
+		bool jump = Input.GetKeyDown (KeyCode.UpArrow);
+		if (jump && true) {
+			rb.AddForce (force);
+		}
+
+		float h = Input.GetAxisRaw("Horizontal");
+
+		gameObject.transform.position = new Vector2 (transform.position.x + (h * speed), 
+			transform.position.y);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+ Unity_Kek2d
