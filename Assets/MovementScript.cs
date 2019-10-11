c6df57676d6b661b11b191e574560fc01efef9a3
Agafiy_Poluchlebnik <zhuravlevyuri2000@gmail.com>
2019-10-11 23:09:12 +0300

    true jump

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -10,6 +10,13 @@ public class MovementScript : MonoBehaviour {
 
 	public GameObject ground;
 
+	//находится ли персонаж на земле или в прыжке?
+	private bool isGrounded = true;
+	//радиус определения соприкосновения с землей
+	private float groundRadius = 0.75f;
+	//ссылка на слой, представляющий землю
+	public LayerMask whatIsGround;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		if (ground == null) {
@@ -18,17 +25,16 @@ public class MovementScript : MonoBehaviour {
 	}
 
 	void Update () {
-
-		//CharacterController controller = GetComponent<CharacterController>();
-		// OnCollisionEnter
-
+		
 		Vector2 force = new Vector2 (0.0f, 15000.0f); 
-		bool jump = Input.GetKeyDown (KeyCode.UpArrow);
-		if (jump && true) {
+		bool jump = Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+		if (jump && Physics2D.OverlapCircle(gameObject.transform.position, groundRadius, whatIsGround)) {
 			rb.AddForce (force);
 		}
 
 		float h = Input.GetAxisRaw("Horizontal");
+
+		print("Кек лол");
 
 		gameObject.transform.position = new Vector2 (transform.position.x + (h * speed), 
 			transform.position.y);
